cc637a43f134ce9c42fae087708328f4e1178891
sonjaerock <kadamon2007@gmail.com>
2017-12-18 20:35:11 +0900

    add basic add delete function

--- app/src/main/java/com/example/rock/todo/MainActivity.java
+++ app/src/main/java/com/example/rock/todo/MainActivity.java
@@ -1,52 +1,81 @@
 package com.example.rock.todo;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    DBHelper dbHelper;
+    ArrayAdapter<String> mAdapter;
+    ListView lstTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        dbHelper = new DBHelper(this);
+        lstTask = (ListView)findViewById(R.id.lstTask);
+        loadTaskList();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    private void loadTaskList() {
+        ArrayList<String> taskList = dbHelper.getTaskList();
+        if(mAdapter==null){
+            mAdapter = new ArrayAdapter<String>(this,R.layout.row,R.id.task_title,taskList);
+            lstTask.setAdapter(mAdapter);
+        }else {
+            mAdapter.clear();
+            mAdapter.addAll(taskList);
+            //mAdapter.notifyDataSetChanged();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_add_task:
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add New Task")
+                        .setMessage("what do you want to do next")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+                                dbHelper.insertNewTask(task);
+                                loadTaskList();
+                            }
+                        })
+                        .setNegativeButton("Cancel",null)
+                        .create();
+                dialog.show();
+                return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    public void deleteTask(View view){
+        View parent = (View)view.getParent();
+        TextView taskTextView = (TextView)parent.findViewById(R.id.task_title);
+        String task = String.valueOf(taskTextView.getText());
+        dbHelper.deleteTask(task);
+        loadTaskList();
     }
 }
