0ef18b5f0085b25ec984822e64751d2abbfb3b61
sonjaerock <kadamon2007@gmail.com>
2017-12-19 20:26:42 +0900

    add drawer menu

--- app/src/main/java/com/example/rock/todo/AdjustSchedule.java
+++ app/src/main/java/com/example/rock/todo/AdjustSchedule.java
@@ -5,16 +5,12 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toast;
 
 
 @SuppressLint("ValidFragment")
--- app/src/main/java/com/example/rock/todo/MainActivity.java
+++ app/src/main/java/com/example/rock/todo/MainActivity.java
@@ -1,24 +1,29 @@
 package com.example.rock.todo;
 
-import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements RegisterSchedule.OnCompleteListener, AdjustSchedule.OnCompleteListener {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, RegisterSchedule.OnCompleteListener, AdjustSchedule.OnCompleteListener {
     private SQLiteDatabase db;
     DBHelper dbHelper;
     ArrayAdapter<String> mAdapter;
@@ -30,6 +35,32 @@ public class MainActivity extends AppCompatActivity implements RegisterSchedule.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //아래코드는 하단에 메시지 띄우기
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+                show();
+                lv1 = (ListView) findViewById(R.id.lstTask);
+                lv1.setAdapter(ca);
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //todo관련
         dbHelper = new DBHelper(this);
         lstTask = (ListView)findViewById(R.id.lstTask);
         loadTaskList();
@@ -75,46 +106,65 @@ public class MainActivity extends AppCompatActivity implements RegisterSchedule.
         lv1.setOnTouchListener(touchListener);
         lv1.setOnScrollListener(touchListener.makeScrollListener());
     }
-
     private void loadTaskList() {
-        ArrayList<String> taskList = dbHelper.getTaskList();
-        if(mAdapter==null){
-            mAdapter = new ArrayAdapter<String>(this,R.layout.row,R.id.task_title,taskList);
-            lstTask.setAdapter(mAdapter);
-        }else {
-            mAdapter.clear();
-            mAdapter.addAll(taskList);
-            //mAdapter.notifyDataSetChanged();
+
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
-        return super.onCreateOptionsMenu(menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.action_add_task:
-                show();
-                lv1 = (ListView) findViewById(R.id.lstTask);
-                lv1.setAdapter(ca);
-                break;
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public void deleteTask(View view){
-        View parent = (View)view.getParent();
-        TextView taskTextView = (TextView)parent.findViewById(R.id.task_title);
-        String task = String.valueOf(taskTextView.getText());
-        dbHelper.deleteTask(task);
-        loadTaskList();
-    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
 
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
     @Override
     public void onInputedData(String title, String contents, String label, String year, String month, String day) {
         Toast.makeText(getApplicationContext(), title+contents+label+year+month+day, Toast.LENGTH_LONG).show();
@@ -135,12 +185,12 @@ public class MainActivity extends AppCompatActivity implements RegisterSchedule.
         lv1 = (ListView) findViewById(R.id.lstTask);
         ca = new CustomListAdapter(this, image_details);
         lv1.setAdapter(ca);
-
     }
+
     void show()
     {
-       RegisterSchedule newFragment = new RegisterSchedule();
-       newFragment.show(getFragmentManager(), "dialog"); //"dialog"라는 태그를 갖는 프래그먼트를 보여준다.
+        RegisterSchedule newFragment = new RegisterSchedule();
+        newFragment.show(getFragmentManager(), "dialog"); //"dialog"라는 태그를 갖는 프래그먼트를 보여준다.
     }
     void showAdjust(int position){
         AdjustSchedule newFragment = new AdjustSchedule( dbHelper.getTaskList().get(position),
